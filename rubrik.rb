3d621a22d4fca6546554641ea4d45f1390df3266
Peter J. Milanese <peterm@rubrik.com>
2017-01-16 15:52:14 -0500

    Houskeeping on the comments

--- rubrik.rb
+++ rubrik.rb
@@ -5,57 +5,56 @@ require 'json'
 require 'pp'
 
 # Simple ruby to authenticate and grab some info., looking to drop auth vars into a . file shortly
-
-rubrik_hash=JSON.parse(File.read(".rubrik/creds.json"))
+rubrik_hash = JSON.parse(File.read('.rubrik/creds.json'))
 
 server = rubrik_hash['server']
 user = rubrik_hash['username']
 pass = rubrik_hash['password']
 
-def get_token(ip,u,p)
-	url = 'https://' + ip + "/"
-	uri = URI.parse(url)
-	http = Net::HTTP.new(uri.host, uri.port)
-	http.use_ssl = true
-	http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-	request = Net::HTTP::Post.new("/api/v1/login")
-	request.add_field('Content-Type', 'application/json')
-	request.body = {'username' => u, 'password' => p}.to_json
-	response = http.request(request)
-	output = JSON.parse(response.body)
-	return output["token"]
+def get_token(ip, u, p)
+    url = 'https://' + ip + '/'
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Post.new('/api/v1/login')
+    request.add_field('Content-Type', 'application/json')
+    request.body = { 'username' => u, 'password' => p }.to_json
+    response = http.request(request)
+    output = JSON.parse(response.body)
+    output['token']
 end
 
-def call_api(server,user,pass,path)
-	#Decided to make one standard call to check/refresh token
-	token = check_auth(server,user,pass)
-	url = 'https://' + server
-	puts "Calling api at #{url}#{path} with token #{token}"
-	uri = URI.parse(url)
-	http = Net::HTTP.new(uri.host, uri.port)
-	http.use_ssl = true
-	http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-	request = Net::HTTP::Get.new(path)
-	request.basic_auth(token,"")
-	response = http.request(request)
-	return JSON.parse(response.body)
+def call_api(server, user, pass, path)
+    # Decided to make one standard call to check/refresh token
+    token = check_auth(server, user, pass)
+    url = 'https://' + server
+    puts "Calling api at #{url}#{path} with token #{token}"
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(path)
+    request.basic_auth(token, '')
+    response = http.request(request)
+    JSON.parse(response.body)
 end
 
-def find_item_by_ip(hash,base,ip,item)
-	return hash[base].select {|h1| h1['ipAddress'] == ip}.first[item]
+def find_item_by_ip(hash, base, ip, item)
+    hash[base].select { |h1| h1['ipAddress'] == ip }.first[item]
 end
 
-#Get a token if needed
-def check_auth(server,user,pass)
-	unless defined? token
-		#Need to secure this stuff!
-		token = get_token(server,user,pass)
-	end
-	return token
+# Get a token if needed
+def check_auth(server, user, pass)
+    unless defined? token
+        # Need to secure this stuff!
+        token = get_token(server, user, pass)
+    end
+    token
 end
-#Get the vm hash from rubrik
-vm_list = call_api(server,user,pass,"/api/v1/vmware/vm")
+# Get the vm hash from rubrik
+vm_list = call_api(server, user, pass, '/api/v1/vmware/vm')
 
-#Search the vm list for id by vm and output the id
-puts find_item_by_ip(vm_list,"data","172.17.60.97",'effectiveSlaDomainId')
-puts find_item_by_ip(vm_list,"data","172.17.60.97",'effectiveSlaDomainName')
+# Search the vm list for any output field by ip and output the result
+puts find_item_by_ip(vm_list, 'data', '172.17.60.97', 'effectiveSlaDomainId')
+puts find_item_by_ip(vm_list, 'data', '172.17.60.97', 'effectiveSlaDomainName')
