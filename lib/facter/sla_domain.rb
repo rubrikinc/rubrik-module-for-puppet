9e99941bc00427b52f4c02602ee8d03367f09933
Peter J. Milanese <peterm@rubrik.com>
2017-02-03 15:16:24 -0500

    module work

--- lib/facter/sla_domain.rb
+++ lib/facter/sla_domain.rb
@@ -1,8 +1,7 @@
 require 'facter'
-Facter.add("sla_domain") do
+Facter.add(:sla_domain) do
   confine :kernel => [ 'Linux' , 'SunOS' , 'FreeBSD' , 'Darwin' ]
   setcode do
-    system('touch /tmp/didit')
     Facter::Util::Resolution.exec("/bin/echo 'here it is!''").chomp
   end
 end
