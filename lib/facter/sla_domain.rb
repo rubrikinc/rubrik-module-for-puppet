b9c9e845d1a8be844381720e044293fc5b0d8bdd
Peter J. Milanese <peterm@rubrik.com>
2017-02-03 16:13:03 -0500

    trying to run facter

--- lib/facter/sla_domain.rb
+++ lib/facter/sla_domain.rb
@@ -1,8 +1,8 @@
 require 'facter'
 Facter.add(:sladomain) do
-#  confine :kernel => [ 'Linux' , 'SunOS' , 'FreeBSD' , 'Darwin' ]
+#  confine :kernel => [ 'Linux' , 'SunOS' , 'FreeBSD' , 'Darwin' ] - for when we get windwos in the loop.
   setcode do
-    #Facter::Core::Execution.exec(‘/bin/echo HELP’)
-    'We should set the sladomain'
+    # Here we want to call the underlying ruby to find the current sla_domain.
+    'We should set the sladomain via :hostname'
   end
 end
