756aec1f5c0243508749904f237a947d2380bc95
Peter Milanese <petermilanese@nypl.org>
2017-02-03 17:18:08 -0500

    fixed sublibs

--- rubrikGetSla.rb
+++ rubrikGetSla.rb
@@ -1,8 +1,9 @@
-require './lib/getVm.rb'
-require './lib/getSlaHash.rb'
-require './lib/setSla.rb'
+require __dir__ + '/lib/getVm.rb'
+require __dir__ + '/lib/getSlaHash.rb'
+require __dir__ + '/lib/setSla.rb'
 # usage setSla.rb [vmname] [desiredSlaName]
 
+puts __dir__
 machineName=ARGV[0]
 if machineName.nil?
   Kernel.abort("Need Hostname")
