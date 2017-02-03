5a1efa3d6281fb94ea492556e1211046177562d2
Peter J. Milanese <peterm@rubrik.com>
2017-02-03 17:02:06 -0500

    simplify getSla

--- lib/getVm.rb
+++ lib/getVm.rb
@@ -2,9 +2,7 @@ require './lib/getFromApi.rb'
 
 # Grab Requested [item] from hash and return ony that value
 def findVmItem(t, item)
-	puts "Grabbing #{item} for #{t}"
 	t = t.upcase
 	h = Hash[getFromApi('/api/v1/vmware/vm?search_value='+t)]
-	pp h
 	return h['data'][0][item]
 end
