69cdcf4354c45e78fc3b67cc6f48a6eefa4cda78
Peter J. Milanese <peterm@rubrik.com>
2017-02-03 14:55:58 -0500

    Moved ntp to hiera

--- lib/facter/sla_domain.rb
+++ lib/facter/sla_domain.rb
@@ -1,5 +1,5 @@
 require 'facter'
-Facter.add("rubrik") do
+Facter.add("rubrik.sla_domain") do
   confine :kernel => [ 'Linux' , 'SunOS' , 'FreeBSD' , 'Darwin' ]
   setcode do
     Facter::Util::Resolution.exec("date '+%a %b %d %T:%N %Z %Y'")
