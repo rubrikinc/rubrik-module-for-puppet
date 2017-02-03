96d2aa7b822c318e1e16b25bb38d61b348ece48c
Peter J. Milanese <peterm@rubrik.com>
2017-02-03 15:12:30 -0500

    module work

--- lib/facter/sla_domain.rb
+++ lib/facter/sla_domain.rb
@@ -1,5 +1,5 @@
 require 'facter'
-Facter.add("rubrik.sla_domain") do
+Facter.add("sla_domain") do
   confine :kernel => [ 'Linux' , 'SunOS' , 'FreeBSD' , 'Darwin' ]
   setcode do
     system('touch /tmp/didit')
--- manifests/sla_domain/set.pp
+++ manifests/sla_domain/set.pp
@@ -1,4 +1,4 @@
 class rubrik::sla_domain::set()
 {
-  notify{"rubrik module set ${rubrik.sla_domain} ":}
+  notify{"rubrik module set ${::sla_domain} ":}
 }
