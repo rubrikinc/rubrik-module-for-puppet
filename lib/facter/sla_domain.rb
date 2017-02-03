2b4049ca351d619b717f094892f442546a24b7f8
Peter J. Milanese <peterm@rubrik.com>
2017-02-03 15:56:28 -0500

    module work

--- lib/facter/sla_domain.rb
+++ lib/facter/sla_domain.rb
@@ -1,9 +1,8 @@
 require 'facter'
 Facter.add(:sladomain) do
 #  confine :kernel => [ 'Linux' , 'SunOS' , 'FreeBSD' , 'Darwin' ]
-  puts "Here's where we do the sla_domain lookup"
   setcode do
     #Facter::Core::Execution.exec(‘/bin/echo HELP’)
-    'TEST'
+    'We should set the sladomain'
   end
 end
