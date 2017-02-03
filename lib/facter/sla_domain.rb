e53680f7288007710f3b190481b995f1705c21b0
Peter J. Milanese <peterm@rubrik.com>
2017-02-03 16:49:34 -0500

    Set SLA

--- lib/facter/sla_domain.rb
+++ lib/facter/sla_domain.rb
@@ -3,9 +3,6 @@ Facter.add(:sladomain) do
 #  confine :kernel => [ 'Linux' , 'SunOS' , 'FreeBSD' , 'Darwin' ] - for when we get windwos in the loop.
   setcode do
     # Here we want to call the underlying ruby to find the current sla_domain.
-    Facter::Core::Execution.exec('ruby ruby-bits/rubrikGetSla.rb $::hostname')
+    Facter::Core::Execution.exec('/opt/puppetlabs/puppet/bin/ruby ruby-bits/rubrikGetSla.rb $::hostname')
   end
-end
-Facter.add(:rubypath) do
-setcode 'which ruby'
 end
