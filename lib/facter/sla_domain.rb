f57aaa6189e6b001961a875a0346bb3a4cebbdcf
Peter J. Milanese <peterm@rubrik.com>
2017-02-05 14:14:13 -0500

    update

--- lib/facter/sla_domain.rb
+++ lib/facter/sla_domain.rb
@@ -3,7 +3,8 @@ Facter.add(:sladomain) do
 #  confine :kernel => [ 'Linux' , 'SunOS' , 'FreeBSD' , 'Darwin' ] - for when we get windwos in the loop.
   setcode do
   # begin
-    Facter::Util::Resolution.exec('/opt/puppetlabs/puppet/bin/ruby ./ruby-bits/rubrikGetSla.rb se-pmilanes-pc01')
+    Facter::Util::Resolution.exec('pwd')
+    #Facter::Util::Resolution.exec('/opt/puppetlabs/puppet/bin/ruby ./ruby-bits/rubrikGetSla.rb se-pmilanes-pc01')
   #  'did not timeout!'
   # rescue Facter::Core::Execution::ExecutionFailure
   #  'timeout!'
