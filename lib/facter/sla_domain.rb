df12286a7b16339d74575f610bb74a0e927b24cb
Peter J. Milanese <peterm@rubrik.com>
2017-02-05 13:35:04 -0500

    update

--- lib/facter/sla_domain.rb
+++ lib/facter/sla_domain.rb
@@ -2,12 +2,12 @@ require 'facter'
 Facter.add(:sladomain) do
 #  confine :kernel => [ 'Linux' , 'SunOS' , 'FreeBSD' , 'Darwin' ] - for when we get windwos in the loop.
   setcode do
-    # Here we want to call the underlying ruby to find the current sla_domain.
-  begin
-    Facter::Core::Execution.exec('/opt/puppetlabs/puppet/bin/ruby ruby-bits/rubrikGetSla.rb se-pmilanes-pc01')
-    'did not timeout!'
-  rescue Facter::Core::Execution::ExecutionFailure
-    'timeout!'
-    end
+'output'
+  # begin
+  #  Facter::Core::Execution.exec('/opt/puppetlabs/puppet/bin/ruby ruby-bits/rubrikGetSla.rb se-pmilanes-pc01')
+  #  'did not timeout!'
+  # rescue Facter::Core::Execution::ExecutionFailure
+  #  'timeout!'
+  #  end
   end
 end
