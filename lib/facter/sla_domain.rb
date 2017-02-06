ed70a2fb8b03785780ea6dac2329eb5c676a3df3
Peter J. Milanese <peterm@rubrik.com>
2017-02-06 07:17:26 -0500

    update

--- lib/facter/sla_domain.rb
+++ lib/facter/sla_domain.rb
@@ -7,7 +7,8 @@ Facter.add(:sladomain) do
   ruby = '/opt/puppetlabs/puppet/bin/ruby'
   cmd = File.join(File.dirname(__FILE__), 'ruby-bits/rubrikGetSla.rb')
   run = ruby + ' ' + cmd + ' se-pmilanes-pc01'
-  Facter::Util::Resolution.exec("${run}")
+  puts run
+  #Facter::Util::Resolution.exec("${run}")
   #  'did not timeout!'
   # rescue Facter::Core::Execution::ExecutionFailure
   #  'timeout!'
