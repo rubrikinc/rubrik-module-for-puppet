d2aaf4e585efd01a821e08f28f0e9a303c77da05
Peter J. Milanese <peterm@rubrik.com>
2017-02-06 20:49:01 -0500

    adding notify

--- lib/facter/sla_domain.rb
+++ lib/facter/sla_domain.rb
@@ -1,17 +1,9 @@
 require 'facter'
 Facter.add(:sladomain) do
-#  confine :kernel => [ 'Linux' , 'SunOS' , 'FreeBSD' , 'Darwin' ] - for when we get windwos in the loop.
   setcode do
-  # begin
-  #
   ruby = '/opt/puppetlabs/puppet/bin/ruby'
   cmd = File.join(File.dirname(__FILE__), 'ruby-bits/rubrikGetSla.rb')
-  run = ruby + ' ' + cmd + ' se-pmilanes-pc01'
-
+  run = ruby + ' ' + cmd + ' se-pmilanes-pc01' # This will be dynamic either by vm name and/or IP
   Facter::Util::Resolution.exec(run)
-  #  'did not timeout!'
-  # rescue Facter::Core::Execution::ExecutionFailure
-  #  'timeout!'
-  #  end
   end
 end
