50821f8db3e5323b5c267a7ffe521004183f163a
Peter J. Milanese <peterm@rubrik.com>
2017-02-07 10:30:46 -0500

    adding action if out of compliance

--- manifests/sla_domain/set.pp
+++ manifests/sla_domain/set.pp
@@ -1,6 +1,6 @@
 class rubrik::sla_domain::set( $sla_domain = 'Undefined')
 {
-  if ::sladomain == $sla_domain{
+  if ::sladomain != $sla_domain{
     notify{"SLA Domain out of compliance (${sla_domain} vs ${::sladomain})": }
   }
   else{
