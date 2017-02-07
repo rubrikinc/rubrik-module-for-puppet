1268349a6f98d443e0cdc58fa5de094d7a48f8a6
Peter J. Milanese <peterm@rubrik.com>
2017-02-07 09:13:29 -0500

    trying to drive config through hiera only

--- manifests/sla_domain/set.pp
+++ manifests/sla_domain/set.pp
@@ -1,4 +1,4 @@
 class rubrik::sla_domain::set( $sla_domain = 'Undefined')
 {
-  notify{"In Rubrik Set Routine for ${::hostname}, I have %{::sladomain} but it should be ${sla_domain}":}
+  notify{"In Rubrik Set Routine for ${::hostname}, I have ${::sladomain} but it should be ${sla_domain}":}
 }
