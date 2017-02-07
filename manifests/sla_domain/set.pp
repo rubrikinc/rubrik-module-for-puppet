5e7ca7b01e815c98d66c32da8bdbdda20faf8a97
Peter J. Milanese <peterm@rubrik.com>
2017-02-07 10:26:58 -0500

    typo on variable name

--- manifests/sla_domain/set.pp
+++ manifests/sla_domain/set.pp
@@ -1,9 +1,9 @@
 class rubrik::sla_domain::set( $sla_domain = 'Undefined')
 {
   if ::sladomain == $sla_domain{
-    notify{"SLA Domain out of compliance (${::sladomain})":}
+    notify{"SLA Domain out of compliance (${::sladomain})": }
   }
   else{
-    notify{"SLA Domain Already set properly (${sla_domain})":}
+    notify{"SLA Domain Already set properly (${sla_domain})": }
   }
 }
