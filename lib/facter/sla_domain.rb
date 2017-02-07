1b9b523f55e47f812ecf9037c7ec66007956531c
Peter J. Milanese <peterm@rubrik.com>
2017-02-06 20:57:19 -0500

    adding notify

--- lib/facter/sla_domain.rb
+++ lib/facter/sla_domain.rb
@@ -1,9 +1,10 @@
 require 'facter'
 Facter.add(:sladomain) do
+  hn = Facter.value(:hostname)
   setcode do
   ruby = '/opt/puppetlabs/puppet/bin/ruby'
   cmd = File.join(File.dirname(__FILE__), 'ruby-bits/rubrikGetSla.rb')
-  run = ruby + ' ' + cmd + ' se-pmilanes-pc01' # This will be dynamic either by vm name and/or IP
+  run = ruby + ' ' + cmd + ' ' + hn # This will be dynamic either by vm name and/or IP
   Facter::Util::Resolution.exec(run)
   end
 end
