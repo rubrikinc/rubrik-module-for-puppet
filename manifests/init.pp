74ef24f0d043e1cce6995852825895603b1222df
Peter J. Milanese <peterm@rubrik.com>
2017-02-08 16:48:25 -0500

    added rubrik login file for facter

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,10 @@
 class rubrik($rubriknode,$rubrikpass,$rubrikuser){
-
+  file { '~/.rubrik':
+    ensure => directory,
+  }
+  file { '~/.rubrik/creds.json':
+    ensure  => file,
+    mode    => '0600',
+    content => template('rubrik/creds_json.erb'),
+    }
 }
