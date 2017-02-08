dcf52edd974edcbfb21b2e014cbe3b5fb7d4217a
Peter J. Milanese <peterm@rubrik.com>
2017-02-08 15:10:54 -0500

    moving to rubrik.rb

--- lib/facter/sla_domain.rb
+++ lib/facter/sla_domain.rb
@@ -3,8 +3,8 @@ Facter.add(:sladomain) do
   hn = Facter.value(:hostname)
   setcode do
   ruby = '/opt/puppetlabs/puppet/bin/ruby'
-  cmd = File.join(File.dirname(__FILE__), 'ruby-bits/rubrikGetSla.rb')
-  run = "#{ruby} #{cmd} #{hn}" # This will be dynamic either by vm name and/or IP
+  cmd = File.join(File.dirname(__FILE__), 'ruby-bits/rubrik.rb')
+  run = "#{ruby} #{cmd} #{hn} --sla --get" # This will be dynamic either by vm name and/or IP
   Facter::Util::Resolution.exec(run)
   end
 end
