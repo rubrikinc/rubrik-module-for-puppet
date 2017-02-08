748314117a75885855032606e59d06b974068a40
Peter J. Milanese <peterm@rubrik.com>
2017-02-08 15:35:33 -0500

    Missed a flag on the cutsom fact :/

--- manifests/sla_domain/set.pp
+++ manifests/sla_domain/set.pp
@@ -4,12 +4,14 @@ class rubrik::sla_domain::set (
 {
   if $::sladomain != $rubriksla{
     notify{"Rubrik SLA Domain out of compliance ${::sladomain}, reapplying ${rubrik::rubrikpass}": }
-    $script = '/opt/puppetlabs/puppet/cache/lib/facter/ruby-bits/rubrik.rb'
+    $script = 'rubrik.rb'
+    $path = '/opt/puppetlabs/puppet/cache/lib/facter/ruby-bits/'
     $ruby = '/opt/puppetlabs/puppet/bin/ruby'
-    $cmd =  "${ruby} ${script} -c ${::hostname} -n ${rubrik::rubriknode} -u ${rubrik::rubrikuser} -p ${rubrik::rubrikpass} --sla --set --sladomain ${rubriksla}"
-    notify{"Running ${cmd}":}
+    $cmd =  "${ruby} ${path}${script} -c ${::hostname} -n ${rubrik::rubriknode} -u ${rubrik::rubrikuser} -p ${rubrik::rubrikpass} --sla --set --sladomain ${rubriksla}"
     exec { 'update-sla':
-      command => $cmd,
+      command   => $cmd,
+      path      => $path,
+      logoutput => on_failure,
     }
   }
   else{
