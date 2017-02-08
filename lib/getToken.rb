a896b63a0558f5bc43d7443bb3f4da7bc2a6ae38
Peter J. Milanese <peterm@rubrik.com>
2017-02-08 13:28:41 -0500

    Added ability to set global login variables in addition to the creds file

--- lib/getToken.rb
+++ lib/getToken.rb
@@ -1,24 +1,35 @@
 # Establish Token for subsequent requests during this run
 # No arguments needed
 
-require 'getCreds.rb'
+require 'json'
 require 'net/https'
 require 'uri'
 
 def get_token()
-  rh = getCreds
-  s = rh['server']
-  u = rh['username']
-  p = rh['password']
-    url = 'https://' + s + '/'
-    uri = URI.parse(url)
-    h = Net::HTTP.new(uri.host, uri.port)
-    h.use_ssl = true
-    h.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    r = Net::HTTP::Post.new('/api/v1/login')
-    r.add_field('Content-Type', 'application/json')
-    r.body = { 'username' => u, 'password' => p }.to_json
-    i = h.request(r)
-    o = JSON.parse(i.body)
-    return o['token'],s
+  if !@s.nil? then
+    sv=@s
+    un=@u
+    pw=@p
+  else
+    require 'getCreds.rb'
+    rh = getCreds
+    sv = rh['server']
+    un = rh['username']
+    pw = rh['password']
+  end
+  puts sv
+  url = 'https://' + sv + '/'
+  uri = URI.parse(url)
+  h = Net::HTTP.new(uri.host, uri.port)
+  h.use_ssl = true
+  h.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  r = Net::HTTP::Post.new('/api/v1/login')
+  r.add_field('Content-Type', 'application/json')
+  r.body = { 'username' => un, 'password' => pw }.to_json
+  puts "posting request for token"
+  puts r
+  i = h.request(r)
+  o = JSON.parse(i.body)
+  puts o
+  return o['token'],sv
 end
--- rubrikFiles.rb
+++ rubrikFiles.rb
@@ -1,3 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../lib/', __FILE__)
 require './lib/getFilesetByHostName.rb'
 
 #Give me a hostname and I'll spit back filesets
