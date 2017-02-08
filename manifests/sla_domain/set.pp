fb5ed82f207e3eddfa8b659dcd8cc23c159b5676
Peter J. Milanese <peterm@rubrik.com>
2017-02-08 15:00:52 -0500

    moving to rubrik.rb

--- manifests/sla_domain/set.pp
+++ manifests/sla_domain/set.pp
@@ -4,13 +4,13 @@ class rubrik::sla_domain::set (
 {
   if $::sladomain != $rubriksla{
     notify{"Rubrik SLA Domain out of compliance ${::sladomain}, reapplying ${rubrik::rubrikpass}": }
-    $script = '/opt/puppetlabs/puppet/cache/lib/facter/ruby-bits/rubrikSetSla.rb'
+    $script = '/opt/puppetlabs/puppet/cache/lib/facter/ruby-bits/rubrik.rb'
     $ruby = '/opt/puppetlabs/puppet/bin/ruby'
-    $cmd =  "${ruby} ${script} ${::hostname} ${rubriksla}"
+    $cmd =  "${ruby} ${script} -c ${::hostname} -n ${rubrik::rubriknode} -u ${rubrik::rubrikuser} -p ${rubrik::rubrikpass} --sla --set --sladomain ${rubriksla}"
     notify{"Running ${cmd}":}
-#    exec { 'update-sla':
-#      command => "${ruby} ${script} ${::hostname} ${rubriksla}",
-#    }
+    exec { 'update-sla':
+      command => cmd,
+    }
   }
   else{
     notify{"Rubrik SLA Domain Already set properly ${rubrik::rubrikpass}": }
