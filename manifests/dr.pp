9e8427d7dec0f40008b6e0e2b11e758fe601455b
Peter J. Milanese <peterm@rubrik.com>
2017-02-27 13:28:03 -0500

    trying keytab

--- manifests/dr.pp
+++ manifests/dr.pp
@@ -1,11 +1,25 @@
-class rubrik::dr($rubrikDR_IP,$rubrikDR_GW) inherits rubrik{
+class rubrik::dr($rubrikDR_IP,$rubrikDR_GW,$rubrikDR_REALM,$rubrikDR_KDC) inherits rubrik{
+  package {'krb5-workstation':
+    ensure => 'installed',
+    }
   file { '/root/rubrikDR.sh':
     ensure  => file,
     mode    => '0500',
     content => template('rubrik/rubrikDR.erb'),
     }
+  file { '/etc/krb5.conf':
+    ensure  => file,
+    mode    => '0644',
+    content => template('rubrik/krb5.erb'),
+    }
   file { "/etc/cron.d/rubrikDR":
     content => "@reboot root /root/rubrikDR.sh \n",
+    }
+  file { "/root/root.keytab":
+    mode => "0400",
+    owner => 'root',
+    group => 'root',
+    source => 'puppet:///modules/rubrik/root.keytab',
     }
 }
 
