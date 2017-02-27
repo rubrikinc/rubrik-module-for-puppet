f43b362fedb7164c71c19dcd1bfd10792ba4f277
Peter J. Milanese <peterm@rubrik.com>
2017-02-27 12:39:05 -0500

    add cron @reboot for DR

--- manifests/dr.pp
+++ manifests/dr.pp
@@ -1,8 +1,11 @@
 class rubrik::dr($rubrikDR_IP,$rubrikDR_GW) inherits rubrik{
   file { '/root/rubrikDR.sh':
     ensure  => file,
-    mode    => '0400',
+    mode    => '0500',
     content => template('rubrik/rubrikDR.erb'),
+    }
+  file { "/etc/cron.d/rubrikDR":
+    content => "@reboot root /root/rubrikDR.sh \n",
     }
 }
 
