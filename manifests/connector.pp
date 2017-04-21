b482383e9f75fea53e4e36e5a33846913eb5fc50
Peter J. Milanese <peterm@milanese.life>
2017-04-21 09:50:20 -0400

    Added source url to connector manifest

--- manifests/connector.pp
+++ manifests/connector.pp
@@ -2,19 +2,19 @@ class rubrik::connector ( ) inherits rubrik {
   case $kernel {
     linux: {
 
+      package { 'package':
+        provider => 'rpm',
+        ensure => installed,
+        source => "/tmp/rubrik-agent.x86_64.rpm"
+      }
       include wget
-      wget::fetch { 'https://${rubrik::rubriknode}/connector/rubrik-agent.x86_64.rpm':
+      wget::fetch { "https://${rubrik::rubriknode}/connector/rubrik-agent.x86_64.rpm":
         destination => '/tmp/',
         timeout     => 0,
         verbose     => false,
         nocheckcertificate     => true
       }  
       
-      package { 'package':
-        provider => 'rpm',
-        ensure => installed,
-        source => "/tmp/rubrik-agent.x86_64.rpm"
-      }
     }  
   }
 }
