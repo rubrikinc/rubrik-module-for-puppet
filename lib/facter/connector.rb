a49f44b66a8c8ac391fa3ba1473c6b77b815cbc1
Peter J. Milanese <peterm@milanese.life>
2017-04-24 14:34:24 -0400

    Added fact for rubrik_backup_service

--- lib/facter/connector.rb
+++ lib/facter/connector.rb
@@ -3,7 +3,7 @@ Facter.add(:rubrik_connector_installed) do
     rubrik_connector_installed = ""
     case Facter.value(:kernel) 
     when 'windows' || 'Windows'
-      rubrik_backup_service = (Get-Service \"Rubrik Backup Service\").status 
+      rubrik_backup_service = (Get-Service "Rubrik Backup Service").status 
     end
   end
 end
