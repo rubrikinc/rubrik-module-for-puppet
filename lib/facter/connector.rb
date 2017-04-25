edcb6f9a382abb86d99b67783b261a42325d36a1
Peter J. Milanese <peterm@milanese.life>
2017-04-24 20:10:20 -0400

    Added fact for rubrik_backup_service

--- lib/facter/connector.rb
+++ lib/facter/connector.rb
@@ -2,7 +2,7 @@ Facter.add(:rubrik_connector) do
   setcode do
     case Facter.value(:kernel) 
     when 'windows' || 'Windows'
-       conn_status = Facter::Util::Resolution::exec('powershell (Get-Service "Rubrik Backup Service" -ErrorAction SilentlyContinue).status') 
+       $conn_status = Facter::Util::Resolution::exec('powershell (Get-Service "Rubrik Backup Service" -ErrorAction SilentlyContinue).status') 
        if conn_status == "Running" then
          true
        else
