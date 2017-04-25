099d35367ed2b2222e70fe3964d19b16cb53e16b
Peter J. Milanese <peterm@milanese.life>
2017-04-24 21:14:11 -0400

    Adding facter for linux agent status

--- lib/facter/connector.rb
+++ lib/facter/connector.rb
@@ -9,7 +9,12 @@ Facter.add(:rubrik_connector) do
         'false'
       end
     when 'linux' || 'Linux'
-      Facter::Util::Resolution::exec('/bin/rpm -q -a | grep telnet | wc -l')
+      conn_status = Facter::Util::Resolution::exec('/bin/rpm -q -a | grep telnet | wc -l')
+      if conn_status == 1 then
+        'true'
+      else
+        'false'
+      end
     end 
   end
 end
