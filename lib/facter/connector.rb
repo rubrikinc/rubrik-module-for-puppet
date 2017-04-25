efa034f3103165fa5f9382dd58ce2e51621381e3
Peter J. Milanese <peterm@milanese.life>
2017-04-24 21:05:21 -0400

    Adding facter for linux agent status

--- lib/facter/connector.rb
+++ lib/facter/connector.rb
@@ -2,11 +2,11 @@ Facter.add(:rubrik_connector) do
   setcode do
     case Facter.value(:kernel) 
     when 'windows' || 'Windows'
-       conn_status = Facter::Util::Resolution::exec("powershell (Get-Service 'Rubrik Backup Service' -ErrorAction SilentlyContinue).status") 
-       if conn_status == "Running" then
-         'true'
-       else
-         'false'
+      conn_status = Facter::Util::Resolution::exec("powershell (Get-Service 'Rubrik Backup Service' -ErrorAction SilentlyContinue).status") 
+      if conn_status == "Running" then
+        'true'
+      else
+        'false'
       end
     when 'linux' || 'Linux'
       Facter::Util::Resolution::exec("rpm -q -a | grep rubrik-agent | wc -l")
