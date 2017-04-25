53bbc2236de8394d39fc0cf84b53f46b4638fbfe
Peter J. Milanese <peterm@milanese.life>
2017-04-25 12:13:57 -0400

    changed to regex for kernel check

--- lib/facter/rubrik_connector.rb
+++ lib/facter/rubrik_connector.rb
@@ -1,14 +1,15 @@
 Facter.add(:rubrik_connector) do
   setcode do
-    case Facter.value(:kernel) 
-    when 'windows' || 'Windows'
+    mykernel = Facter.value(:kernel) 
+    case mykernel
+    when /(W|w)indows/ 
       conn_status = Facter::Util::Resolution::exec("powershell (Get-Service 'Rubrik Backup Service' -ErrorAction SilentlyContinue).status") 
       if conn_status == "Running" then
         'true'
       else
         'false'
       end
-    when 'linux' || 'Linux'
+    when /(L|l)inux/ 
       'running linux'
       conn_status = Facter::Util::Resolution::exec("/bin/rpm -q -a | grep telnet | wc -l")
       if conn_status == 1 then
