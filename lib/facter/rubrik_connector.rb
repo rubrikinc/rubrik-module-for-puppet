0ec0b5eb9366da173f78c6b9232a7b9d25b226d7
Peter J. Milanese <peterm@milanese.life>
2017-04-25 12:05:15 -0400

    changed to regex for kernel check

--- lib/facter/rubrik_connector.rb
+++ lib/facter/rubrik_connector.rb
@@ -9,7 +9,7 @@ Facter.add(:rubrik_connector) do
         'false'
       end
     when 'linux' || 'Linux'
-      conn_status = Facter::Util::Resolution::exec('/bin/rpm -q -a | grep telnet | wc -l')
+      Facter::Util::Resolution::exec('/bin/rpm -q -a | grep telnet | wc -l')
 #      if conn_status then
 #        'true'
 #      else
