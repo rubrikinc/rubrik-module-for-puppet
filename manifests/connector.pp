5e445c6d5f01fb50a642d1144088f65e4d82a091
Peter J. Milanese <peterm@milanese.life>
2017-04-25 11:20:58 -0400

    Enabled installation of linux agent

--- manifests/connector.pp
+++ manifests/connector.pp
@@ -18,7 +18,7 @@ class rubrik::connector ( ) inherits rubrik {
       }
     }
   
-    'linux' || 'Linux': {
+    'Linux' || 'Linux': {
       include wget
       wget::fetch { "https://${rubrik::rubriknode}/connector/rubrik-agent.x86_64.rpm":
         destination => '/tmp/',
