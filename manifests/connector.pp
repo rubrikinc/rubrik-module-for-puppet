a7fc9951bc5ea06b42aec643c2ed6ac5ed4dacec
Peter J. Milanese <peterm@milanese.life>
2017-04-25 17:22:23 -0400

    working the Rakefile

--- manifests/connector.pp
+++ manifests/connector.pp
@@ -6,29 +6,27 @@ class rubrik::connector ( ) inherits rubrik {
           url   => "https://${rubrik::rubriknode}/connector/RubrikBackupService.zip",
           destination_directory => $rubrik_temp_dir,
           insecure => true,
-        } ->
-        windows::unzip { "$rubrik_temp_dir\RubrikBackupService.zip":
+        }
+        -> windows::unzip { "$rubrik_temp_dir\RubrikBackupService.zip":
           destination => $rubrik_temp_dir,
           unless     => '$rubrik_temp_dir\RubrikBackupService.msi',
-        } ->
-        package { 'Rubrik Backup Service':
+        }
+        -> package { 'Rubrik Backup Service':
           ensure   => installed,
           source   => "$rubrik_temp_dir\RubrikBackupService.msi",
         }
       }
     }
-  
     /(L|l)inux/: {
-      if $rubrik_connector == 'false' { 
+      if $rubrik_connector == 'false' {
         include wget
         wget::fetch { "https://${rubrik::rubriknode}/connector/rubrik-agent.x86_64.rpm":
           destination => '/tmp/',
           timeout     => 0,
           verbose     => true,
           nocheckcertificate     => true
-        } -> 
-    
-        package { 'rubrik-agent':
+        }
+        -> package { 'rubrik-agent':
           provider => 'rpm',
           ensure   => installed,
           source   => "/tmp/rubrik-agent.x86_64.rpm"
