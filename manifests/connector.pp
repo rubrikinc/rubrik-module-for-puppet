64b4cfd8e286c63a19c8d96750a94a103696dccc
Peter J. Milanese <peterm@milanese.life>
2017-04-25 17:53:13 -0400

    solving lint issues

--- manifests/connector.pp
+++ manifests/connector.pp
@@ -1,38 +1,38 @@
 class rubrik::connector ( ) inherits rubrik {
   case $::kernel {
     /(W|w)indows/: {
-      if $::rubrik_connector == 'false' {
+      if $::rubrik_connector == false {
         download_file { 'Rubrik Agent Download' :
-          url   => "https://${rubrik::rubriknode}/connector/RubrikBackupService.zip",
+          url                   => "https://${rubrik::rubriknode}/connector/RubrikBackupService.zip",
           destination_directory => $::rubrik_temp_dir,
-          insecure => true,
+          insecure              => true,
         }
-        -> windows::unzip { "${rubrik_temp_dir}\RubrikBackupService.zip":
+        -> windows::unzip { "${::rubrik_temp_dir}\RubrikBackupService.zip":
           destination => $::rubrik_temp_dir,
-          unless     => '$rubrik_temp_dir\RubrikBackupService.msi',
+          unless      => '$rubrik_temp_dir\RubrikBackupService.msi',
         }
         -> package { 'Rubrik Backup Service':
-          ensure   => installed,
-          source   => "${rubrik_temp_dir}\RubrikBackupService.msi",
+          ensure => installed,
+          source => "${::rubrik_temp_dir}\RubrikBackupService.msi",
         }
       }
     }
     /(L|l)inux/: {
-      if $::rubrik_connector == 'false' {
+      if $::rubrik_connector == false {
         include wget
         wget::fetch { "https://${rubrik::rubriknode}/connector/rubrik-agent.x86_64.rpm":
-          destination => '/tmp/',
-          timeout     => 0,
-          verbose     => true,
-          nocheckcertificate     => true
+          destination        => '/tmp/',
+          timeout            => 0,
+          verbose            => true,
+          nocheckcertificate => true
         }
         -> package { 'rubrik-agent':
-          provider => 'rpm',
           ensure   => installed,
+          provider => 'rpm',
           source   => '/tmp/rubrik-agent.x86_64.rpm'
         }
       }
-    }  
+    }
     default: {
       fail("Module ${module_name} has no current value on ${::operatingsystem}")
     }
