cc5ec08d354d607b372a7f5e6bfa0442ad65d672
Peter Milanese <petermilanese@nypl.org>
2017-04-26 12:35:24 -0400

    Fixed so that \ warnings dont occur

--- spec/classes/rubrik-linux_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'rubrik' do
-  let(:node) { 'se-pmilanes-pc01' }
-  let(:facts) { {:operatingsystem => 'RedHat', :ipaddress => '192.168.0.1', :kernel => 'Linux'} }
-  let(:params) { {:rubriknode => '1.2.3.4', :rubrikuser => 'user', :rubrikpass => 'password'} }
-  it { is_expected.to compile }
-  it { is_expected.to contain_file('/root/.rubrik/creds.json') }
-end
