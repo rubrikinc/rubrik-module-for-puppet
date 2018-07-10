bc3851476c124d931256b8697200d6a89fd73100
Peter J. Milanese <peterm@milanese.life>
2018-07-10 10:49:08 -0400

    update to tie to local

--- lib/facter/ruby-bits/lib/getVm.rb
+++ lib/facter/ruby-bits/lib/getVm.rb
@@ -4,7 +4,7 @@ require 'getFromApi.rb'
 # Grab Requested [item] from hash and return ony that value
 def findVmItem(t, item)
 	t = t.upcase
-	h = Hash[getFromApi('/api/v1/vmware/vm?is_relic=false&name='+t)]
+	h = Hash[getFromApi('/api/v1/vmware/vm?is_relic=false&name='+t+'&primary_cluster_id=local')]
 #	puts h['data'][0][item]
 	return h['data'][0][item]
 end
